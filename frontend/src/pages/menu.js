ce58d3471dd171c407200da734fc9b9b3426b92e
TY <mr.tyeung@gmail.com>
2022-07-24 16:26:20 -0700

    feat: added a background and styled the menu item price & name

--- frontend/src/pages/menu.js
+++ frontend/src/pages/menu.js
@@ -4,7 +4,7 @@
 const Menu = () => {
   return (
     // <ul>{MenuItems.categories.map(cat => <Category name={cat.name} />)}</ul>
-    <section id="tabs">
+    <section class="h-screen bg-dark-aliona bg-no-repeat bg-cover bg-fixed" id="tabs">
       <div class="w-screen mx-auto my-6 mb-32 mt-12 px-6">
         <div
           class="flex flex-col justify-center mx-auto mb-6 border-b md:space-x-10 md:flex-row"
@@ -13,29 +13,31 @@ const Menu = () => {
             class="flex justify-center text-center cursor-pointer text-gray-600 border-b md:border-b-0 hover:text-softRed md:w-1/3 tab"
             data-target="panel-1"
           >
-            <div class="py-5 border-b-4 border-softRed" data-target="panel-1">Bakery</div>
+            <div class="py-5 text-white border-b-4 border-softRed" data-target="panel-1">Bakery</div>
           </div>
 
           <div
             class="flex justify-center text-center cursor-pointer text-gray-600 border-b md:border-b-0 hover:text-softRed md:w-1/3 tab"
             data-target="panel-2"
           >
-            <div class="py-5" data-target="panel-2">Entrees</div>
+            <div class="py-5 text-white" data-target="panel-2">Entrees</div>
           </div>
 
           <div class="flex justify-center text-center cursor-pointer text-gray-600 border-b md:border-b-0 hover:text-softRed md:w-1/3 tab" data-target="panel-3" >
-            <div class="py-5" data-target="panel-3">Drinks</div>
+            <div class="py-5 text-white" data-target="panel-3">Drinks</div>
           </div>
         </div>
 
         <div id="panels" class="container mx-auto">
           <div class="flex flex-col py-5 md:flex-row md:space-x-7 panel panel-1">
-            <div class="flex justify-center md:w-1/2">
+            <div class="relative flex justify-center md:w-1/2">
               <img
-                src="images/70c2a6247e7b58.jpg"
+                src="images/95d02a230fe050.jpg"
                 alt="bakery"
-                class="relative z-10 rounded-full "
+                class="relative rounded-full "
               />
+              <h3 class="absolute bg-white text-slate-800 bottom-3 rounded-full px-1 drop-shadow z-5">$2.99</h3>
+              <h3 class="absolute text-white -bottom-8" >Muffin</h3>
             </div>
             <div class="flex justify-center md:w-1/2">
               <img
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,6 +14,11 @@ module.exports = {
       colors: {
         softBlue: "hsl(204, 56%, 59%)",
         darkSlate: "hsl(213, 10%, 22%)",
+      },
+      backgroundImage: {
+        'dark-tumeric': "url('../public/images/background/lucas-marulier--tpMfp_GmTI-unsplash.jpg')",
+        'dark-aliona': "url('../public/images/background/aliona-gumeniuk-AOQ83RPea60-unsplash.jpg')",
+        'dark-pickled-stardust': "url('../public/images/background/pickled-stardust-4xc6i5BKPWs-unsplash.jpg')",
       }
     },
   },
