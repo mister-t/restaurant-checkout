90b62dd876a66f0458ae4e601332542178db71f4
TY <mr.tyeung@gmail.com>
2022-07-24 23:17:43 -0700

    feat: added 'order' icon from https://heroicons.com/

--- frontend/src/pages/menu.js
+++ frontend/src/pages/menu.js
@@ -133,6 +133,9 @@ const Menu = () => {
           <article className="h-1/3 text-white flex flex-col  text-center"><button className="h-full w-full border-y text-3xl tracking-wider">Cancel</button>
           </article>
           <article className="h-2/3 flex flex-col items-center justify-center text-white">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 my-2" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clipRule="evenodd" />
+            </svg>
             <button className="min-h-2/3 text-white text-4xl tracking-wider font-bold mb-5">ORDER</button> 
             <h1 className="text-2xl tracking-wider ">$55.99</h1>
           </article>
