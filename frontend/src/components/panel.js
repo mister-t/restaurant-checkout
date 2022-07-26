77d4d5f0ae19b1810a82be8af256a7c6faa0bce5
TY <mr.tyeung@gmail.com>
2022-07-26 15:29:13 -0700

    refactor: added a 'flex col' to center the item names

--- frontend/src/components/panel.js
+++ frontend/src/components/panel.js
@@ -5,16 +5,16 @@ const Panel = ({item, numOfItems}) => {
   const itemSize = 2;
 
   return (
-      <div data-category-id={catId} data-item-id={itemId} className={`panel basis-1/${itemSize} relative border-2`}>
+    <div data-category-id={catId} data-item-id={itemId} className={`panel basis-1/${itemSize} relative border-0 flex flex-col justify-center items-center`}>
         <img
           src={imgSrc}
           alt={name}
           title={name}
           className="rounded-full h-40 w-40"
         />
-        <h3 className="absolute bg-white text-slate-800 right-1/3 bottom-5 rounded-full px-1 mx-8 drop-shadow">${normalizedPrice}</h3>
+        <h3 className="absolute bg-white text-slate-800 right-1/3 bottom-5 rounded-full px-1 drop-shadow">${normalizedPrice}</h3>
         <h3 className="absolute text-white -bottom-8" >{name}</h3>
-      </div>
+    </div>
   )
 }
 
