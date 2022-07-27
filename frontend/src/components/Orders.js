0b9f2718acb8ec37a236403149405edd01cf345c
TY <mr.tyeung@gmail.com>
2022-07-26 22:55:14 -0700

    refactor: added overflow for vertical contents

--- frontend/src/components/Orders.js
+++ frontend/src/components/Orders.js
@@ -1,7 +1,7 @@
 const Orders = () => {
   return (
-    <section className="flex flex-col fixed bottom-0 w-screen min-h-1/4 mx-auto md:flex-row">
-      <div className="w-2/3 mx-0 p-1 bg-white">
+    <section className="flex flex-col fixed bottom-0 w-screen max-h-52 mx-auto md:flex-row">
+      <div className="w-2/3 mx-0 p-1 bg-white/90 overflow-y-scroll">
         <article className="relative flex flex-col border p-4">
           <div className="flex justify-between">
             <h1 className="text-xl font-bold">Impossible Burger</h1>
