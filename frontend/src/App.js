54ad28850ea2f3d014187bd28cc46eab06bccf94
TY <mr.tyeung@gmail.com>
2022-07-27 16:04:25 -0700

    feat: added routers to the front end in order to connect to the backend later

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 // import Header from "./components/layout/Header";
 // import Footer from "./components/layout/Footer";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Menu from "./pages/Menu";
 
 const App =  () => {
   return (
-    <>
-      <Menu />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Menu/>} exact />
+      </Routes>
+    </Router>
   );
 }
 
