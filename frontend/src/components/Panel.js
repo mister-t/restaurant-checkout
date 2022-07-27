4885a05b0e20665f7123841cbf87f1fddafda060
TY <mr.tyeung@gmail.com>
2022-07-27 09:07:47 -0700

    refactor: minimized the food panel box and added the image white border back

--- frontend/src/components/Panel.js
+++ frontend/src/components/Panel.js
@@ -5,14 +5,14 @@ const Panel = ({item, numOfItems}) => {
   const itemSize = 2;
 
   return (
-    <div data-category-id={catId} data-item-id={itemId} className={`panel basis-1/${itemSize} relative border-0 flex flex-col justify-center items-center`}>
+    <div data-category-id={catId} data-item-id={itemId} className={`panel basis-1/${itemSize} relative h-40 border-0 flex flex-col justify-center items-center`}>
         <img
           src={imgSrc}
           alt={name}
           title={name}
-          className="rounded-full h-40 w-40"
+          className="rounded-full h-40 w-40 border-2"
         />
-        <h3 className="absolute bg-white text-slate-800 bottom-5 rounded-full px-1 drop-shadow">${normalizedPrice}</h3>
+        <h3 className="absolute bg-white text-slate-800 bottom-2 rounded-full px-1 drop-shadow">${normalizedPrice}</h3>
         <h3 className="absolute text-white -bottom-8" >{name}</h3>
     </div>
   )
