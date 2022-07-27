77f323c66f21d7a4fd46f6920aa545c495d44c98
TY <mr.tyeung@gmail.com>
2022-07-26 22:07:30 -0700

    refactor: added bg color and opacity to the orders

--- frontend/src/components/Orders.js
+++ frontend/src/components/Orders.js
@@ -33,13 +33,13 @@ const Orders = () => {
         </article>
       </div>
       <div className="w-1/3">
-        <article className="h-1/3 text-white flex items-center justify-center border-y">
+        <article className="bg-slate-800/90 h-1/3 text-white flex items-center justify-center border-y">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
           </svg>
           <button className="text-3xl tracking-wider">Cancel</button>
         </article>
-        <article className="h-2/3 flex flex-col items-center justify-center text-white">
+        <article className="bg-slate-800/70 h-2/3 flex flex-col items-center justify-center text-white">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 my-2" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clipRule="evenodd" />
           </svg>
