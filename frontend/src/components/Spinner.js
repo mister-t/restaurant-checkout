dd4425a4af4d85e3e9edd01281655311be306963
TY <mr.tyeung@gmail.com>
2022-07-31 19:58:18 -0700

    fix: class names for spinner

--- frontend/src/components/Spinner.js
+++ frontend/src/components/Spinner.js
@@ -1,7 +1,7 @@
 const Spinner = () => {
   return (
-    <div class="h-screen flex items-center justify-center">
-      <div class="w-40 h-40 border-l-2 border-gray-900 rounded-full animate-spin"></div>
+    <div className="h-screen flex items-center justify-center">
+      <div className="w-40 h-40 border-l-2 border-gray-900 rounded-full animate-spin"></div>
     </div> 
   )
 };
