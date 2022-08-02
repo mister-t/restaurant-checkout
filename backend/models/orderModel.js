8307590c78d895d2637d83ddee04d4c78766a9f7
TY <mr.tyeung@gmail.com>
2022-08-02 14:24:55 -0700

    refactor: udpated the Order schema and model

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,31 +1,22 @@
 import mongoose from 'mongoose';
+import ItemSchema from './itemModel.js';
 
 const orderSchema = mongoose.Schema({
-  'orderItems': [{
-    'name': { type: String, trim: true, required: true },
-    
-    'price': { type: Number, required: true },
-    'quantity': { type: Number, required: true },
-    'image': {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'Image'
-    },
-    'item': {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'Item'
-    },
-  }],
+  'orderItems': [
+    {
+      'item': ItemSchema,
+      'quantity': { type: Number, required: true },
+    }
+  ],
   'payment': {
-    'cardType': { type: String, trim: true, required: true },
+    'cardType': { type: String, trim: true, required: false },
     'nameOnCard': { type: String, trim: true, required: true },
     'expirationMonth': { type: Number, required: true },
     'expirationYear': { type: Number, required: true },
     'cvc': { type: Number, required: true },
   },
   'totalAmount': {
-    type: String, trim: true, required: true
+    type: Number, required: true
   },
 }, { timestamps: true });
 
