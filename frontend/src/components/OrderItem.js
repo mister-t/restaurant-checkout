cee353bd6d088f4a2a58dddbd5b6f489e98b99cc
TY <mr.tyeung@gmail.com>
2022-08-02 11:22:10 -0700

    feat: added increment and decrement buttons for an order item

--- frontend/src/components/OrderItem.js
+++ frontend/src/components/OrderItem.js
@@ -14,7 +14,11 @@ const Order = ({ id, name, description, price, qty }) => {
     <article className="relative flex flex-col border p-4">
       <div className="flex justify-between">
         <h1 className="text-xl font-bold">{name}</h1>
-        <h2 className="pr-5">${price.toFixed(2)} X {qty}</h2>
+        <span className="flex items-center justify-center mr-16">
+          <span className='inline-block h-10 w-10 text-3xl text-white text-center rounded-full bg-darkSlate'>-</span>
+          <span className="px-5">${price.toFixed(2)} X {qty}</span>
+          <span className='inline-block h-10 w-10 text-3xl text-white text-center rounded-full bg-darkSlate'>+</span>
+        </span>
       </div>
       <p className="text-slate-400">{description}</p>
       <div className="absolute bottom-0 right-0 w-11 overflow-hidden inline-block cursor-pointer" onClick={onRemoveItemHandler}>
