a0ebbac2e513d2d352dee4b6fb58173c1ef24648
TY <mr.tyeung@gmail.com>
2022-08-02 14:34:09 -0700

    refactor: preload the background image before rendering to avoid the white background

--- backend/dbSeeder.js
+++ backend/dbSeeder.js
@@ -10,6 +10,7 @@ import { itemsToImg } from './data/items.js';
 import Image from './models/imageModel.js';
 import Category from './models/categoryModel.js';
 import Item from './models/itemModel.js';
+import Order from '../models/orderModel.js';
 
 import connectDB from './config/db.js';
 connectDB(); //connect to our db cluster in Mongodb.com
@@ -21,31 +22,32 @@ const importData = async () => {
     await Category.deleteMany();
     await Image.deleteMany();
     await Item.deleteMany();
+    await Order.deleteMany();
 
     const createdImages = await Image.insertMany(images);
 
     //match created images to categories
     const catsToInsert = createdImages.filter(img => categoriesImages[img.name])
-    .map(img => ({ name: categoriesImages[img.name], image: img._id }));
+      .map(img => ({ name: categoriesImages[img.name], image: img._id }));
     const createdCategories = await Category.insertMany(catsToInsert);
 
     //Match the category names with their newly created IDs
-    const categoryIDs = createdCategories.map(cat => ({[cat.name]: cat._id})).reduce((result, cat) => ({...result, ...cat}), {});
+    const categoryIDs = createdCategories.map(cat => ({ [cat.name]: cat._id })).reduce((result, cat) => ({ ...result, ...cat }), {});
 
     //Match newly created images & categories to the items
     const itemsToInsert = createdImages.filter(img => itemsToImg[img.name])
-    .map(image => {
-      const item = itemsToImg[image.name];
-      const { category, name, price } = item;
-      return ({category: categoryIDs[category], name, price, image });
-    });
+      .map(image => {
+        const item = itemsToImg[image.name];
+        const { category, name, price } = item;
+        return ({ category: categoryIDs[category], name, price, image });
+      });
     // console.log(JSON.stringify(itemsToInsert))
     const insertedItems = await Item.insertMany(itemsToInsert);
 
 
     console.log(`Data imported successfully`.bgGreen);
     process.exit();
-  } catch(err) {
+  } catch (err) {
     console.log(`Data import failed:
       ${err.message}`.bgRed);
     process.exit(1);
@@ -61,14 +63,14 @@ const dropData = async () => {
 
     console.log(`Data deleted successfuly`.bgGreen);
     process.exit();
-  } catch(err) {
+  } catch (err) {
     console.log(`Data deletion failed: 
       ${err.message}`.bgRed);
     process.exit(1);
   }
 };
 
-if(process.argv[2] === DROP_DATA) {
+if (process.argv[2] === DROP_DATA) {
   //example run script: node data:drop -d
   dropData();
 } else {
