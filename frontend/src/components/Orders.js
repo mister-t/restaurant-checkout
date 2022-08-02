3f3d12fab82c25fc14e4e0da4845d9b35dc3559c
TY <mr.tyeung@gmail.com>
2022-08-01 22:39:36 -0700

    refactor: added items are shown in the order section

--- frontend/src/components/Orders.js
+++ frontend/src/components/Orders.js
@@ -1,34 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import OrderItem from './OrderItem';
 import OrderCancel from './OrderCancelBtn';
 import OrderSubmitBtn from './OrderSubmitBtn';
+import { useEffect } from 'react';
 
 const Orders = () => {
-  const orders = [
-    {
-      id: 1,
-      name: 'Beyond Impossible Burger',
-      description: 'Wholewheat Bun, Gouda Cheese, Lettuce',
-      price: 10.00
-    },
-    {
-      id: 2,
-      name: 'Curly Fries',
-      description: '',
-      price: 5.00
-    },
-    {
-      id: 3,
-      name: 'Double-Cheese Burger',
-      description: 'Quarter-pound Wagyu beef, Onions',
-      price: 15.00
-    },
-  ];
+  const { items } = useSelector(state => state.cart);
 
   return (
     <section className="flex flex-col w-screen max-h-52 mx-auto md:flex-row">
       <div className="w-2/3 mx-0 p-1 bg-white/90 overflow-y-scroll">
         {
-          orders.map(order => <OrderItem key={order.id} name={order.name} id={order.id} description={order.description} price={order.price} />)
+          items.map(item => <OrderItem key={item.id} name={item.name} id={item.id} description={item.description} price={item.price} />)
         }
       </div>
       <div className="w-1/3">
