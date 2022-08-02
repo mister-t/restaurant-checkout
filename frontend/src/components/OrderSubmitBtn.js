48ccc35eb9dcc959d8416d8408f1377bf67e7a05
TY <mr.tyeung@gmail.com>
2022-08-02 00:13:08 -0700

    feat: added ability to tally the total amount in the cart

--- frontend/src/components/OrderSubmitBtn.js
+++ frontend/src/components/OrderSubmitBtn.js
@@ -1,5 +1,11 @@
-const OrderSubmitBtn = () => {
+import { useSelector } from 'react-redux';
 
+const OrderSubmitBtn = () => {
+  const { total } = useSelector(state => state.cart);
+
+  if (total > 0) {
+    console.log(`cart total = ${total}`)
+  }
   const onClickHandler = (evt) => {
     evt.preventDefault();
     console.log(`order is submitted`);
@@ -11,7 +17,8 @@ const OrderSubmitBtn = () => {
         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clipRule="evenodd" />
       </svg>
       <button className="min-h-2/3 text-white text-4xl tracking-wider font-bold mb-5">ORDER</button>
-      <h1 className="text-2xl tracking-wider ">$55.99</h1>
+      <h1 className="text-2xl tracking-wider ">${total.toFixed(2)}</h1>
+
     </article>
   )
 }
--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -6,7 +6,11 @@ import {
 export const cartReducer = (state = { items: [], total: 0 }, action) => {
   switch (action.type) {
     case ADD_ITEM_TO_CART:
-      return { items: [action.payload, ...state.items] };
+      const cartItems = [action.payload, ...state.items];
+      return {
+        items: cartItems,
+        total: cartItems.reduce((acc, item) => acc + item.price, 0)
+      };
     case REMOVE_ITEM_FROM_CART:
       return {
         ...state,
