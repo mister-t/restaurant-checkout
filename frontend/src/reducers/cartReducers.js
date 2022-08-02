80873115fe99cc5731bcf807612518f6729d12fb
TY <mr.tyeung@gmail.com>
2022-08-02 00:26:04 -0700

    fix: reduce the price when items are removed

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -6,17 +6,19 @@ import {
 export const cartReducer = (state = { items: [], total: 0 }, action) => {
   switch (action.type) {
     case ADD_ITEM_TO_CART:
-      const cartItems = [action.payload, ...state.items];
+      const addedItems = [action.payload, ...state.items];
       return {
-        items: cartItems,
-        total: cartItems.reduce((acc, item) => acc + item.price, 0)
+        items: addedItems,
+        total: addedItems.reduce((acc, item) => acc + item.price, 0)
       };
     case REMOVE_ITEM_FROM_CART:
+      const cartItems = state.items.filter(item => {
+        return item.id !== action.payload.id
+      });
       return {
         ...state,
-        items: state.items.filter(item => {
-          return item.id !== action.payload.id
-        })
+        items: cartItems,
+        total: cartItems.reduce((acc, item) => acc + item.price, 0)
       }
     default:
       return state;
