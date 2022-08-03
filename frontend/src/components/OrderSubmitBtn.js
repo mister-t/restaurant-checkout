9b11b63e70db4a299e564ca2cc134da34806e841
TY <mr.tyeung@gmail.com>
2022-08-03 10:19:57 -0700

    refactor: stacked the order items, Order btn, and Cancel btn in mobile view

--- frontend/src/components/OrderSubmitBtn.js
+++ frontend/src/components/OrderSubmitBtn.js
@@ -9,13 +9,12 @@ const OrderSubmitBtn = () => {
   };
 
   return (
-    <article className="bg-slate-800/70 flex flex-col-reverse md:flex-col items-center justify-center text-white cursor-pointer" onClick={onClickHandler}>
+    <article className="bg-slate-800/70 flex flex-col-reverse items-center justify-center text-white cursor-pointer md:flex-col" onClick={onClickHandler}>
       <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 md:h-48 md:w-48 my-2 sm:hidden" viewBox="0 0 20 20" fill="currentColor">
         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clipRule="evenodd" />
       </svg>
       <button className="min-h-2/3 text-white text-4xl tracking-wider font-bold mb-5">ORDER</button>
       <h1 className="text-2xl tracking-wider ">${total.toFixed(2)}</h1>
-
     </article>
   )
 }
