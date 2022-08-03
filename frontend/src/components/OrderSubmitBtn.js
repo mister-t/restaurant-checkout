a5c9d17685ae24c859713dc1bcb4f7eaab4b35e8
TY <mr.tyeung@gmail.com>
2022-08-03 13:37:17 -0700

    refactor: added more height to the Orders section and fixed the Order icon to appear on md screen and above

--- frontend/src/components/OrderSubmitBtn.js
+++ frontend/src/components/OrderSubmitBtn.js
@@ -9,8 +9,8 @@ const OrderSubmitBtn = () => {
   };
 
   return (
-    <article className="bg-slate-800/70 flex flex-col-reverse items-center justify-center text-white cursor-pointer md:flex-col py-2 md:py-4" onClick={onClickHandler}>
-      <svg xmlns="http://www.w3.org/2000/svg" className="hidden h-12 w-12 md:h-48 md:w-48 my-2 md:display" viewBox="0 0 20 20" fill="currentColor">
+    <article className="bg-slate-800/70 flex flex-col-reverse items-center justify-center text-white cursor-pointer md:flex-col py-2 md:py-2" onClick={onClickHandler}>
+      <svg xmlns="http://www.w3.org/2000/svg" className="hidden md:h-12 md:w-12 my-2 md:block" viewBox="0 0 20 20" fill="currentColor">
         <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clipRule="evenodd" />
       </svg>
       <button className="min-h-2/3 text-white text-5xl tracking-wider font-bold mb-5 md:text-4xl">ORDER</button>
--- frontend/src/components/Orders.js
+++ frontend/src/components/Orders.js
@@ -8,7 +8,7 @@ const Orders = () => {
   const { items } = useSelector(state => state.cart);
 
   return (
-    <section className="flex flex-col w-screen mx-auto md:max-h-52 md:flex-row">
+    <section className="flex flex-col w-screen mx-auto md:max-h-1/3 md:flex-row">
       <div className="mx-0 p-1 bg-white/90 overflow-y-scroll md:w-2/3 md:p-1">
         {
           items.length ? items.map(item => <OrderItem key={item.id} name={item.name} id={item.id} description={item.description} price={item.price} qty={item.qty} />) : <div className='h-full flex items-center justify-center'><h1 className='text-5xl text-darkSlate text-opacity-30'>Please add items</h1></div>
