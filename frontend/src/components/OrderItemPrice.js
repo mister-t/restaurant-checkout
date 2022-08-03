b4d3e6dad9a629692f49db46431aaa067cae26d6
TY <mr.tyeung@gmail.com>
2022-08-03 11:53:10 -0700

    refactor: font-normal on desktop

--- frontend/src/components/OrderItemPrice.js
+++ frontend/src/components/OrderItemPrice.js
@@ -18,7 +18,7 @@ const OrderItemPrice = ({ id, price, qty }) => {
     <span className='md:w-1/2'>
       <span className={`inline-block h-12 w-12 text-4xl md:h-10 md:w-10 md:text-3xl text-white text-center rounded-full cursor-pointer select-none ${qty > 1 ? 'bg-darkSlate' : 'bg-slate-200'}`} onClick={onDecrementHandler}>-</span>
 
-      <span className="mx-2 text-xl font-bold w-30 md:mx-6">${price.toFixed(2)} X {qty}</span>
+      <span className="mx-2 text-xl font-bold w-40 md:mx-6 md:font-normal">${price.toFixed(2)} X {qty}</span>
       <span className='inline-block h-12 w-12 text-4xl md:h-10 md:w-10 md:text-3xl text-white text-center rounded-full bg-darkSlate cursor-pointer select-none' onClick={onIncrementHandler}>+</span>
     </span>
   )
