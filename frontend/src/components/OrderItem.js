9543509fcc686eda9ca20e6285c2faf504052a89
TY <mr.tyeung@gmail.com>
2022-08-03 12:05:03 -0700

    refactor: more style chagnes to the item price so that it look proper in both mobile and desktop

--- frontend/src/components/OrderItem.js
+++ frontend/src/components/OrderItem.js
@@ -11,9 +11,9 @@ const Order = ({ id, name, description, price, qty }) => {
   };
 
   return (
-    <article className="relative flex flex-col border p-4">
-      <div className="flex justify-between">
-        <h1 className="text-xl font-bold">{name}</h1>
+    <article className="relative flex flex-col border py-2 md:p-4">
+      <div className="flex justify-start md:justify-between">
+        <h1 className="text-xl font-bold w-20 break-words">{name}</h1>
         <OrderItemPrice id={id} price={price} qty={qty} />
       </div>
       <p className="text-slate-400">{description}</p>
--- frontend/src/components/OrderItemPrice.js
+++ frontend/src/components/OrderItemPrice.js
@@ -15,10 +15,9 @@ const OrderItemPrice = ({ id, price, qty }) => {
   };
 
   return (
-    <span className='md:w-1/2'>
+    <span className='md:mr-6'>
       <span className={`inline-block h-12 w-12 text-4xl md:h-10 md:w-10 md:text-3xl text-white text-center rounded-full cursor-pointer select-none ${qty > 1 ? 'bg-darkSlate' : 'bg-slate-200'}`} onClick={onDecrementHandler}>-</span>
-
-      <span className="mx-2 text-xl font-bold w-40 md:mx-6 md:font-normal">${price.toFixed(2)} X {qty}</span>
+      <span className="inline-block text-center mx-2 text-xl font-bold w-32 md:mx-6 md:font-normal">${price.toFixed(2)} X {qty}</span>
       <span className='inline-block h-12 w-12 text-4xl md:h-10 md:w-10 md:text-3xl text-white text-center rounded-full bg-darkSlate cursor-pointer select-none' onClick={onIncrementHandler}>+</span>
     </span>
   )
