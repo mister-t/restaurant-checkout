40e3c099ba07ba0c9fc6e539ec797ae004613603
TY <mr.tyeung@gmail.com>
2022-08-03 11:49:41 -0700

    refactor: integrated the new order item price component

--- frontend/src/components/OrderItem.js
+++ frontend/src/components/OrderItem.js
@@ -1,5 +1,6 @@
 import { useDispatch } from 'react-redux';
-import { removeItemFromCart, incrementItemQty, decrementItemQty } from '../actions/cartActions';
+import { removeItemFromCart } from '../actions/cartActions';
+import OrderItemPrice from './OrderItemPrice';
 
 const Order = ({ id, name, description, price, qty }) => {
   const dispatch = useDispatch();
@@ -9,26 +10,11 @@ const Order = ({ id, name, description, price, qty }) => {
     dispatch(removeItemFromCart({ name, id }));
   };
 
-  const onDecrementHandler = (evt) => {
-    evt.preventDefault();
-    dispatch(decrementItemQty(id));
-  };
-
-  const onIncrementHandler = (evt) => {
-    evt.preventDefault();
-    dispatch(incrementItemQty(id));
-  };
-
   return (
     <article className="relative flex flex-col border p-4">
       <div className="flex justify-between">
         <h1 className="text-xl font-bold">{name}</h1>
-        <span className="flex items-center justify-center mr-16">
-          <span className={`inline-block h-10 w-10 text-3xl text-white text-center rounded-full cursor-pointer select-none ${qty > 1 ? 'bg-darkSlate' : 'bg-slate-200'}`} onClick={onDecrementHandler}>-</span>
-
-          <span className="mx-6 w-20">${price.toFixed(2)} X {qty}</span>
-          <span className='inline-block h-10 w-10 text-3xl text-white text-center rounded-full bg-darkSlate cursor-pointer select-none' onClick={onIncrementHandler}>+</span>
-        </span>
+        <OrderItemPrice id={id} price={price} qty={qty} />
       </div>
       <p className="text-slate-400">{description}</p>
       <div className="absolute bottom-0 right-0 w-11 overflow-hidden inline-block cursor-pointer" onClick={onRemoveItemHandler}>
