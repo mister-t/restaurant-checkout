e0c79c2a5c8269277496a9e79695a8575950051e
TY <mr.tyeung@gmail.com>
2022-08-05 16:11:33 -0700

    refactor: removed width for the cc radio btn

--- frontend/src/components/orders/OrderSubmitBtn.js
+++ frontend/src/components/orders/OrderSubmitBtn.js
@@ -75,11 +75,11 @@ const OrderSubmitBtn = () => {
           <h1 className="ff-mpr1c-regular text-slate-500 text-2xl md:text-3xl tracking-tight font-bold mb-8 pt-2">Payment Information</h1>
           <form className="flex flex-col border-2 p-8 rounded-md">
             <div class="flex justify-between mb-10">
-              <section className="w-1/2 flex justify-start">
+              <section className="flex justify-start">
                 <input className="accent-mgPurple focus:outline-none" id="add-new-credit-card" type="radio" name="cardType" value="Add a new card" checked />
                 <label className="ml-2">Add a new card</label>
               </section>
-              <section className="w-1/2 flex justify-end">
+              <section className="flex justify-end">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
                 </svg>
@@ -94,7 +94,7 @@ const OrderSubmitBtn = () => {
               <div className="flex justify-between rounded-md border-2">
                 <input className="w-1/3 rounded-md pl-[10px] py-[4px] focus:outline-none" id="full-name" type="text" placeholder="Card number" />
                 <section className="">
-                  <input id="cc-month" maxLength={2} placeholder="MM" className="w-7 focus:outline-none py-[4px]" />
+                  <input id="cc-month" type="number" maxLength={2} placeholder="MM" className="w-7 focus:outline-none py-[4px]" />
                   <span className='text-slate-800/50 mx-1 w-2'>/</span>
                   <input id="cc-year" maxLength={2} placeholder="YY" className="w-7 pr-1 focus:outline-none py-[4px]" />
                   <input id="cc-cvc" maxLength={3} placeholder="CVC" className="w-8 mx-2 focus:outline-none py-[4px]" />
