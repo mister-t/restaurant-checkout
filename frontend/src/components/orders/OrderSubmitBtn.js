2363a9cfddca551db5532de6adce4f280853a2c9
TY <mr.tyeung@gmail.com>
2022-08-05 17:31:32 -0700

    fix: typos

--- frontend/src/components/orders/OrderSubmitBtn.js
+++ frontend/src/components/orders/OrderSubmitBtn.js
@@ -45,6 +45,11 @@ const OrderSubmitBtn = () => {
     // });
   };
 
+  const onSubmit = evt => {
+    evt.preventDefault();
+    console.log('form submitted')
+  }
+
   useEffect(() => {
     if (choice) {
       console.log(`The answer is Yes: ${Date.now()}`)
@@ -64,7 +69,7 @@ const OrderSubmitBtn = () => {
         ">${total.toFixed(2)}</h1>
       </article>
       {modalVisible && <Modal >
-        <div class="flex justify-end px-8 -pt-2 sm:px-0">
+        <div className="flex justify-end px-8 -pt-2 sm:px-0">
           <button className="text-slate-500 font-bold focus:outline-none" onClick={onCancelHandler}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
@@ -73,31 +78,31 @@ const OrderSubmitBtn = () => {
         </div>
         <div className="flex flex-col items-center w-screen sm:w-full">
           <h1 className="ff-mpr1c-regular text-slate-500 text-2xl md:text-3xl tracking-tight font-bold mb-8 pt-2">Payment Information</h1>
-          <form className="flex flex-col border-2 p-8 rounded-md">
-            <div class="flex justify-between mb-10">
+          <form onSubmit={onSubmit} className="flex flex-col border-2 p-8 rounded-md">
+            <div className="flex justify-between mb-10">
               <section className="flex justify-start">
-                <input className="accent-mgPurple focus:outline-none" id="add-new-credit-card" type="radio" name="cardType" value="Add a new card" checked />
+                <input className="accent-mgPurple focus:outline-none" id="add-new-credit-card" type="radio" name="cardType" value="Add a new card" defaultChecked />
                 <label className="ml-2">Add a new card</label>
               </section>
               <section className="flex justify-end">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z" />
                 </svg>
               </section>
             </div>
-            <div class="flex flex-col items-start mb-5">
+            <div className="flex flex-col items-start mb-5">
               <label className="mb-2">Name on card</label>
-              <input className="w-full rounded-md pl-[10px] py-[4px] border-2 focus:outline-none " id="full-name" type="text" placeholder="John Doe" />
+              <input className="w-full rounded-md pl-[10px] py-[4px] border-2 focus:outline-none " id="full-name" type="text" placeholder="John Doe" required />
             </div>
             <div>
-              <label className="inline-block text-left w-full mb-2" for="full-name">Card number</label>
+              <label className="inline-block text-left w-full mb-2" htmlFor="full-name">Card number</label>
               <div className="flex flex-col md:flex-row justify-between rounded-md border-2">
                 <input className="w-1/2 rounded-md pl-[10px] py-[4px] focus:outline-none" id="full-name" type="text" placeholder="Card number" />
                 <section className="">
-                  <input id="cc-month" maxLength={2} placeholder="MM" className="w-7 focus:outline-none py-[4px]" />
+                  <input id="cc-month" maxLength={2} placeholder="MM" className="w-7 focus:outline-none py-[4px]" required />
                   <span className='text-slate-800/50 mx-1 w-2'>/</span>
-                  <input id="cc-year" maxLength={2} placeholder="YY" className="w-7 pr-1 focus:outline-none py-[4px]" />
-                  <input id="cc-cvc" maxLength={3} placeholder="CVC" className="w-8 mx-2 focus:outline-none py-[4px]" />
+                  <input id="cc-year" maxLength={2} placeholder="YY" className="w-7 pr-1 focus:outline-none py-[4px]" required />
+                  <input id="cc-cvc" maxLength={3} placeholder="CVC" className="w-8 mx-2 focus:outline-none py-[4px]" required />
                 </section>
               </div>
             </div>
