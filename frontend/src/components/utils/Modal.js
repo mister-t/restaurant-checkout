ce87cc2b203af96a9de651e146ec777dbacf8701
TY <mr.tyeung@gmail.com>
2022-08-07 14:04:52 -0700

    adjusted the payment screen height

--- frontend/src/components/utils/Modal.js
+++ frontend/src/components/utils/Modal.js
@@ -1,8 +1,8 @@
 const Modal = ({ children }) => {
   return (
     <div className="modal-overlay bg-white/95 fixed inset-0 z-50">
-      <div className="modal flex items-center justify-center h-screen overflow-y-scroll">
-        <div className="flex-col justify-between bg-white text-center py-12 px-24 rounded-md md:py-6 md:px-6 md:w-1/2">
+      <div className="modal flex flex-col items-center justify-center h-screen  overflow-y-scroll overflow-x-hidden">
+        <div className="bg-white text-center sm:py-12 py-6 px-24 rounded-md md:py-6 md:px-6 md:w-1/2">
           {children}
         </div>
       </div>
