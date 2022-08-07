976bbe7087377a6e74106353d50733d6ccb8942b
TY <mr.tyeung@gmail.com>
2022-08-07 12:35:29 -0700

    refactor: changed the modal overlay to white

--- frontend/src/components/utils/Modal.js
+++ frontend/src/components/utils/Modal.js
@@ -1,6 +1,6 @@
 const Modal = ({ children }) => {
   return (
-    <div className="modal-overlay bg-slate-800/80 fixed inset-0 z-50">
+    <div className="modal-overlay bg-white/95 fixed inset-0 z-50">
       <div className="modal flex items-center justify-center h-screen overflow-y-scroll">
         <div className="flex-col justify-between bg-white text-center py-12 px-24 rounded-md md:py-6 md:px-6 md:w-1/2">
           {children}
