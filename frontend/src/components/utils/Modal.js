ec3a5548df8fb202f6f82811c0f3652a4b091e4c
TY <mr.tyeung@gmail.com>
2022-08-07 14:17:23 -0700

    refactor: added a small drop shadow to the payment screen

--- frontend/src/components/utils/Modal.js
+++ frontend/src/components/utils/Modal.js
@@ -1,7 +1,7 @@
 const Modal = ({ children }) => {
   return (
     <div className="modal-overlay bg-white/95 fixed inset-0 z-50">
-      <div className="modal flex flex-col items-center justify-center h-screen  overflow-y-scroll overflow-x-hidden">
+      <div className="modal flex flex-col items-center justify-center h-screen  overflow-y-scroll overflow-x-hidden drop-shadow-sm">
         <div className="bg-white text-center sm:py-12 py-6 px-24 rounded-md md:py-6 md:px-6 md:w-1/2">
           {children}
         </div>
