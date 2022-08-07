79f9fd65c4ebf3c28dfb5ace8e72598fb0c97163
TY <mr.tyeung@gmail.com>
2022-08-07 12:22:18 -0700

    refactor: added padding top to the order buttons section when in smaller screen size

--- frontend/src/components/orders/OrderSubmitBtn.js
+++ frontend/src/components/orders/OrderSubmitBtn.js
@@ -104,7 +104,7 @@ const OrderSubmitBtn = () => {
 
   return (
     <>
-      <article className="bg-slate-800/70 flex flex-col-reverse items-center justify-center text-white cursor-pointer select-none md:flex-col md:py-2" onClick={onPayOrder}>
+      <article className="bg-slate-800/70 flex flex-col-reverse items-center justify-center text-white cursor-pointer select-none pt-3 md:flex-col md:py-2" onClick={onPayOrder}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-20 w-20 md:h-12 md:w-12 my-2 md:block" viewBox="0 0 20 20" fill="currentColor">
           <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clipRule="evenodd" />
         </svg>
