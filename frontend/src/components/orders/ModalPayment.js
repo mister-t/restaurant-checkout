072b786b52948a97a121cad50a6c191db8e7f0ad
TY <mr.tyeung@gmail.com>
2022-08-08 09:41:42 -0700

    refactor: extracted payment values on the form

--- frontend/src/components/orders/ModalPayment.js
+++ frontend/src/components/orders/ModalPayment.js
@@ -7,6 +7,7 @@ import OrderPaymentCancelBtn from './OrderPaymentCancelBtn'
 const ModalPayment = ({ title = "Payment Information", paymentValues, onHandlers }) => {
   const { loading, success } = useSelector(state => state.orderCreate);
   const { onFormInputChange, onSubmit, onCancelHandler } = onHandlers;
+  const { cardType, fullName, ccNumber, expMonth, expYear, cvc } = paymentValues;
 
   return (
     <Modal>
@@ -17,7 +18,7 @@ const ModalPayment = ({ title = "Payment Information", paymentValues, onHandlers
           <div className="flex flex-col border-2 p-8 rounded-md">
             <div className="flex justify-between mb-10">
               <section className="flex justify-start">
-                <input className="accent-mgPurple focus:outline-none" id="add-new-credit-card" type="radio" defaultChecked name="cardType" value={paymentValues.cardType} onChange={onFormInputChange} />
+                <input className="accent-mgPurple focus:outline-none" id="add-new-credit-card" type="radio" defaultChecked name="cardType" value={cardType} onChange={onFormInputChange} />
                 <label className="ml-2">Add a new card</label>
               </section>
               <section className="flex justify-end">
@@ -28,17 +29,17 @@ const ModalPayment = ({ title = "Payment Information", paymentValues, onHandlers
             </div>
             <div className="flex flex-col items-start mb-5">
               <label className="mb-2">Name on card</label>
-              <input className="w-full rounded-md pl-[10px] py-[4px] border-2 focus:outline-none " id="full-name" type="text" placeholder="John Doe" required maxLength={128} name="fullName" value={paymentValues.fullName} onChange={onFormInputChange} />
+              <input className="w-full rounded-md pl-[10px] py-[4px] border-2 focus:outline-none " id="full-name" type="text" placeholder="John Doe" required maxLength={128} name="fullName" value={fullName} onChange={onFormInputChange} />
             </div>
             <div>
               <label className="inline-block text-left w-full mb-2" htmlFor="full-name">Card number</label>
               <div className="flex flex-col md:flex-row justify-between rounded-md border-2">
-                <input className="w-1/2 rounded-md pl-[10px] py-[4px] focus:outline-none" id="full-name" type="text" minLength={12} maxLength={16} placeholder="Card number" required name="ccNumber" value={paymentValues.ccNumber} onChange={onFormInputChange} />
+                <input className="w-1/2 rounded-md pl-[10px] py-[4px] focus:outline-none" id="full-name" type="text" minLength={12} maxLength={16} placeholder="Card number" required name="ccNumber" value={ccNumber} onChange={onFormInputChange} />
                 <section className="">
-                  <input id="cc-month" maxLength={2} placeholder="MM" className="w-7 focus:outline-none py-[4px]" required name="expMonth" value={paymentValues.expMonth} onChange={onFormInputChange} />
+                  <input id="cc-month" maxLength={2} placeholder="MM" className="w-7 focus:outline-none py-[4px]" required name="expMonth" value={expMonth} onChange={onFormInputChange} />
                   <span className='text-slate-800/50 mx-1 w-2'>/</span>
-                  <input id="cc-year" maxLength={2} placeholder="YY" className="w-7 pr-1 focus:outline-none py-[4px]" required name="expYear" value={paymentValues.expYear} onChange={onFormInputChange} />
-                  <input id="cc-cvc" minLength={3} maxLength={4} placeholder="CVC" className="w-8 mx-2 focus:outline-none py-[4px]" required name="cvc" value={paymentValues.cvc} onChange={onFormInputChange} />
+                  <input id="cc-year" maxLength={2} placeholder="YY" className="w-7 pr-1 focus:outline-none py-[4px]" required name="expYear" value={expYear} onChange={onFormInputChange} />
+                  <input id="cc-cvc" minLength={3} maxLength={4} placeholder="CVC" className="w-8 mx-2 focus:outline-none py-[4px]" required name="cvc" value={cvc} onChange={onFormInputChange} />
                 </section>
               </div>
             </div>
