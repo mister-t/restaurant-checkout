ff4343c923c93dd642532b374540f5d20c982dcb
TY <mr.tyeung@gmail.com>
2022-08-07 22:18:56 -0700

    refactor: added a spinner when order is submitted

--- frontend/src/components/utils/ModalPayment.js
+++ frontend/src/components/utils/ModalPayment.js
@@ -1,8 +1,9 @@
 import { useSelector } from 'react-redux';
 import Modal from './Modal';
+import Spinner from '../utils/Spinner';
 
 const ModalPayment = ({ title = "Payment Information", paymentValues, onHandlers }) => {
-  const { loading, success, order } = useSelector(state => state.orderCreate);
+  const { loading, success } = useSelector(state => state.orderCreate);
 
   return (
     <Modal>
@@ -20,7 +21,7 @@ const ModalPayment = ({ title = "Payment Information", paymentValues, onHandlers
           </svg>
           <h1 class="text-4xl font-bold">Thank You !</h1>
           <p>Please keep your receipt. It will be needed when your order is ready.</p>
-          <a href="#" onClick={onHandlers.onCancelHandler} class="inline-flex items-center px-4 py-2 text-white bg-mgPurple border border-mgPurple rounded hover:bg-indigo-700 focus:outline-none focus:ring">
+          <button onClick={onHandlers.onCancelHandler} class="inline-flex items-center px-4 py-2 text-white bg-mgPurple border border-mgPurple rounded hover:bg-indigo-700 focus:outline-none focus:ring">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3 mr-2" fill="none" viewBox="0 0 24 24"
               stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M7 16l-4-4m0 0l4-4m-4 4h18" />
@@ -28,9 +29,9 @@ const ModalPayment = ({ title = "Payment Information", paymentValues, onHandlers
             <span class="text-sm font-medium">
               New Order
             </span>
-          </a>
+          </button>
         </div>
-      ) : <div className="flex flex-col items-center w-screen sm:w-full">
+      ) : loading ? <Spinner /> : <div className="flex flex-col items-center w-screen sm:w-full">
         <h1 className="ff-mpr1c-regular text-slate-500 text-2xl md:text-3xl tracking-tight font-bold mb-8 pt-2">{title}</h1>
         <form onSubmit={onHandlers.onSubmit}>
           <div className="flex flex-col border-2 p-8 rounded-md">
