455baf7a22dfcd42b632dc6080dd19499c5f1ff5
TY <mr.tyeung@gmail.com>
2022-08-08 18:54:15 -0700

    refactor: added receipt number/ID at the thankyou page

--- frontend/src/components/orders/OrderThankyou.js
+++ frontend/src/components/orders/OrderThankyou.js
@@ -1,12 +1,15 @@
-const OrderThankyou = ({ onCancelHandler }) => {
+import { useSelector } from 'react-redux';
 
+const OrderThankyou = ({ onCancelHandler }) => {
+  const { order } = useSelector(state => state.orderCreate);
+  const { _id } = order;
   return (
     <div className="flex flex-col items-center space-y-2">
       <svg xmlns="http://www.w3.org/2000/svg" className="text-green-600 w-28 h-28" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="1">
         <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
       </svg>
       <h1 className="text-4xl font-bold">Thank You !</h1>
-      <p>Please keep your receipt. It will be needed when your order is ready.</p>
+      <p>Please keep your receipt. You'll be notified when your order, <strong>#{`${String(_id).substring(0, 6)}`}</strong>, is ready.</p>
       <button tabIndex={1} onClick={onCancelHandler} className="inline-flex items-center px-4 py-2 text-white bg-mgPurple border border-mgPurple rounded hover:bg-indigo-700 focus:outline-none focus:ring">
         <svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 mr-2" fill="none" viewBox="0 0 24 24"
           stroke="currentColor" strokeWidth="2">
